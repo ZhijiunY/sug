aa49b161c081bc1b25ef2de7d623e24383670811
ZhijiunY <yzj90596@gmail.com>
2023-10-05 13:02:01 +0800

    顯示明細、登記收入

--- main.go
+++ main.go
@@ -10,9 +10,22 @@ func main() {
 	//聲明一個變量，控制是否退出for循環
 	loop := true
 
+	// 定義帳戶餘額
+	balance := 10000.0
+
+	// 每次收支金額
+	money := 0.0
+
+	// 每次收支說明
+	note := ""
+
+	// 收支的詳情用字符串紀錄
+	// 當有收支時，只需要對details 進行拼接處理即可
+	details := "收支\t帳戶金額\t收支金額\t說明"
+
 	// 顯示主菜單
 	for {
-		fmt.Println("------------家庭收支記帳------------")
+		fmt.Println("\n------------家庭收支記帳------------")
 		fmt.Println("		1 收支明細")
 		fmt.Println("		2 登記收入")
 		fmt.Println("		3 登記支出")
@@ -24,7 +37,17 @@ func main() {
 		switch key {
 		case "1":
 			fmt.Println("------------當前收支明細------------")
+			fmt.Println(details)
 		case "2":
+			fmt.Println("本次收入金額：")
+			fmt.Scan(&money)
+			// 修改帳戶餘額
+			balance += money
+			fmt.Println("本次收入說明：")
+			fmt.Scan(&note)
+			// 將收入情況拼接到 details 變量
+			// 收入  1100   1000  有人發紅包
+			details += fmt.Sprintf("\n收入\t%v\t%v\t%v", balance, money, note)
 		case "3":
 			fmt.Println("登記支出..")
 		case "4":
